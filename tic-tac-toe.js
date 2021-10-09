cd6a2a36f1a12dd757f006a52830c98cdb9fb286
Patrick Witter <pwitterop123@gmail.com>
2021-10-09 10:37:47 -0500

    Fixed Bugs - Game Fully Implemented

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,16 +3,18 @@ window.addEventListener('load', (e) => {
 
     let Oselected = 1
     let Xselected = 0;
-    let Unselected = -1;
-    let Gamestate = Unselected;
+    let Gamestate = -1;
 
 
     let squares = document.querySelectorAll("#board > div");
     let bton = document.querySelector(".btn");
 
     let status = document.getElementById("status");
+    let defStatus = status.innerHTML;
     var boardStatus = Array(9).fill(0);
     let selectedboard = 0;
+    let gameOver = false;
+
 
     squares.forEach((sqr) => {
         sqr.classList.add("square");
@@ -24,26 +26,26 @@ window.addEventListener('load', (e) => {
 
     squares.forEach((sqr, index) => {
         sqr.onclick = (e) => {
-            if (sqr.innerHTML == "<p></p>") {
-                Gamestate = Math.floor(Math.random() * 0);
+            if (sqr.innerHTML == "<p></p>" && !gameOver) {
+                Gamestate = Math.floor(Math.random() * 2); // Randomly assign 0:X or 1:O
 
-                if (Gamestate == Unselected || Gamestate == Oselected) {
-                    sqr.classList.remove("O");
-                    sqr.classList.add("X");
-                    sqr.innerHTML = 'X';
+                if (Gamestate == Oselected) {
+                    // sqr.classList.remove("O");
+                    sqr.classList.add("O");
+                    sqr.innerHTML = 'O';
                     Gamestate = Xselected;
-                    boardStatus[index] = "X"
-                    console.log(`Gamestate: Xselected ${Gamestate}`);
+                    boardStatus[index] = "O"
+                    // console.log(`Gamestate: Oselected ${Gamestate}`);
                     selectedboard++
 
                 }
                 else {
-                    sqr.classList.remove("X");
-                    sqr.classList.add("O");
-                    sqr.innerHTML = 'O';
+                    // sqr.classList.remove("X");
+                    sqr.classList.add("X");
+                    sqr.innerHTML = 'X';
                     Gamestate = Oselected;
-                    boardStatus[index] = "O"
-                    console.log(`Gamestate: Oselected ${Gamestate}`);
+                    boardStatus[index] = "X"
+                    // console.log(`Gamestate: Xselected ${Gamestate}`);
                     selectedboard++
                 }
             }
@@ -56,12 +58,20 @@ window.addEventListener('load', (e) => {
                 console.log(`top: ${top} \ntop: ${middle} \ntop: ${bottom} \n`);
 
                 if (top == "XXX" || middle == "XXX" || bottom == "XXX") {
+                    console.log(`top: ${top} middle: ${middle} bottom: ${bottom}`);
                     status.innerHTML = "Congratulations! X is the Winner!";
                     status.className = "you-won";
+                    gameOver = true;
                 }
                 if (top == "OOO" || middle == "OOO" || bottom == "OOO") {
                     status.innerHTML = "Congratulations! O is the Winner!";
                     status.className = "you-won";
+                    gameOver = true;
+                }
+                if (selectedboard == 9) {
+                    status.innerHTML = "Congratulations! X and O are the Winners!";
+                    status.className = "you-won";
+                    gameOver = true;
                 }
             }
         }
@@ -80,8 +90,12 @@ window.addEventListener('load', (e) => {
 
     bton.addEventListener('click', (event) => {
         squares.forEach((sq) => sq.innerHTML = "<p></p>");
-        console.log(boardStatus);
+        // console.log(boardStatus);
         boardStatus = Array(9).fill(0);
+        gameOver = false;
+        status.innerHTML = defStatus;
+        status.className = "";
+        selectedboard = 0;
     });
 
 
